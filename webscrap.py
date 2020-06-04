421bf940da6684d3c35a8ef850af32781d99c9c1
bartquSubo <barsub@wp.pl>
2020-06-04 08:42:11 +0100

    restoring older ver of script. added: error exceptions, proxy and headers toggling, logging to a file

--- webscrap.py
+++ webscrap.py
@@ -1,31 +1,42 @@
-import re
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
+import pandas as pd
+import re
 from time import sleep
+import random
 from random import randint
+import logging
+from itertools import cycle
+from lxml.html import fromstring
+# for entire alphabet:
 import string
 
+
+# error logging configuration
+logging.basicConfig(filename='/home/subo/PycharmProjects/webscrap_tekstowo/errors.log', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+logger = logging.getLogger(__name__)
+
 records = []
-# alphabet = ['B']
-alphabet = list(string.ascii_uppercase)
+alphabet = ['C', 'D']
+# alphabet = list(string.ascii_uppercase)
 
 
-def get_general_urls():
-    for letter in alphabet:
-        general_url = 'https://www.tekstowo.pl/artysci_na,' + letter
-        # number of sub-pages to scrap:
-        for i in range(1, 10):
-            url_page = general_url + ',strona,' + str(i) + '.html'
-            # print(url_page)
-            return url_page
+def get_proxies():
+    """
+    get a free proxy list and randomize it
+    :return: lst of proxy addresses
+    """
 
-
-def get_specific_url(imported_tag, regex_pattern):
-    for x in clean_list(imported_tag):
-        remove_leading = re.sub(regex_pattern, "", x)
-        text = normalize_list(remove_leading)
-        return text
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
 
 
 def clean_list(clean_soup):
@@ -37,6 +48,7 @@ def clean_list(clean_soup):
     :param clean_soup:
     :return:
     """
+
     # treat the parsed tag as html
     soup_clean = BeautifulSoup(clean_soup.text, "lxml")
     # convert the content tag to lowercase str
@@ -55,7 +67,7 @@ def normalize_list(clean_string):
     :param clean_string: str
     :return: str; clean_trailing_under
     """
-    # clean_numb = re.sub(r"\d+\. ", "", clean_string)
+
     clean_str = re.sub(r"\(\d+ utworów\)", "", clean_string)
     clean_str = clean_str.replace("&amp;", "__")
     clean_punctuation = re.sub(r"[^\w\s]", "_", clean_str)
@@ -69,21 +81,33 @@ def soup_url(request_url, tag, tag_class, tag_element):
     """
     Function to request.get a URL and convert it to a BeautifulSoup object.
     :param request_url: str
-    :param div_tag: str
-    :param class_tag: str
-    :param element_tag: str
+    :param tag: str
+    :param tag_class: str
+    :param tag_element: str
     :return: str; tags
     """
-    r = requests.get(request_url)
-    # to get Polish diacritics correctly written:
-    r.encoding = r.apparent_encoding
-    soup = BeautifulSoup(r.text, 'lxml')
-    # get the specific tag: element
-    found = soup.find(tag, {tag_class: tag_element})
-    return found
+
+    for user_agent in range(1, 10):
+        # use proxy addresses:
+        proxy = next(proxy_pool)
+        proxies = {"http://": proxy, "https://": proxy}
+        # use various user_Agent headers
+        user_agent = random.choice(user_agent_list)
+        s = requests.Session()
+        s.max_redirects = 60
+        s.headers['User-Agent'] = user_agent
+        # headers = {"User-Agent": user_agent}
+        r = s.get(request_url, proxies=proxies, headers=s.headers, allow_redirects=3)
+        # to get Polish diacritics correctly written:
+        r.encoding = r.apparent_encoding
+        soup = BeautifulSoup(r.text, 'lxml')
+        # get the specific tag: element
+        found = soup.find(tag, {tag_class: tag_element})
+        return found
 
 
 def soup_song(song, tag):
+
     soup = BeautifulSoup(song.text, 'lxml')
     # get the specific tag: element
     found = soup.find(tag)
@@ -92,94 +116,76 @@ def soup_song(song, tag):
     clean_trailing_text = re.sub(r"Poznaj historię zmian tego tekstu \n\n</p>", "", clean_leading_text)
     return clean_trailing_text
 
-################################################################
-'''
+
+proxies = get_proxies()
+proxy_pool = cycle(proxies)
+
+user_agent_list = [
+    # Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    # Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
 for letter in alphabet:
     general_url = 'https://www.tekstowo.pl/artysci_na,' + letter
+
     # number of sub-pages to scrap:
-    for i in range(1, 2):
+    for i in range(1, 6):
         url_page = general_url + ',strona,' + str(i) + '.html'
-        '''
+        artists_tag = soup_url(url_page, "div", "class", "content")
+        for x in clean_list(artists_tag):
+            artist_remove_initial = re.sub(r"\d+\. ", "", x)
+            artist_name = normalize_list(artist_remove_initial)
 
-# get artist URLs
-artists_tag = soup_url(get_general_urls(), "div", "class", "content")
-artist_name = get_specific_url(artists_tag, r"\d+\. ")
-artist_url = 'https://www.tekstowo.pl/piosenki_artysty,' + artist_name + '.html'
+            # create artists urls
+            artist_url = 'https://www.tekstowo.pl/piosenki_artysty,' + artist_name + '.html'
+            songs_tag = soup_url(artist_url, "div", "class", "ranking-lista")
+            # print(artist_name)
 
-# get song URLs
-songs_tag = soup_url(artist_url, "div", "class", "ranking-lista")
-song_title = get_specific_url(songs_tag, r"\d+\. \w.* - ")
-song_url = 'https://www.tekstowo.pl/piosenka,' + artist_name + ',' + song_title + '.html'
+            for y in clean_list(songs_tag):
+                song_remove_initial = re.sub(r"\d+\. \w.* - ", "", y)
+                song_title = normalize_list(song_remove_initial)
+                song_url = 'https://www.tekstowo.pl/piosenka,' + artist_name + ',' + song_title + '.html'
+                text_tag = soup_url(song_url, "div", "class", "song-text")
+                try:
+                    song_text = soup_song(text_tag, "p")
+                    translation_tag = soup_url(song_url, "div", "id", "translation")
+                    translation_text = soup_song(translation_tag, "p")
+                except (requests.exceptions.TooManyRedirects, requests.exceptions.ConnectionError, requests.TooManyRedirects, AttributeError) as attr:
+                    logger.error(attr)
+                    print("AttributeError:NoneType error for " + song_url)
+                else:
+                    records.append((song_text, translation_text, song_title, artist_name, song_url))
+                    print(song_url)
+                    # needed for not getting blocked on a server:
+                    sleep(randint(2, 9))
 
-# def text_translation():
+######################
+# save the crawled data to a DataFrame:
+df = pd.DataFrame(records, columns=['song_text', 'translation_text', 'song_title', 'artist_name', 'song_url'])
+df.to_csv('tekstowo_data.csv', index=False, encoding='utf-8')
 
-# get song text and translation
-song_tag = soup_url(song_url, "div", "class", "song-text")
-song_text = soup_song(song_tag, "p")
-
-translation_tag = soup_url(song_url, "div", "id", "translation")
-translation_text = soup_song(translation_tag, "p")
-print(song_url)
-################################################################
-'''
-for x in clean_list(artists_tag):
-    artist_remove_initial = re.sub(r"\d+\. ", "", x)
-    artist_name = normalize_list(artist_remove_initial)
-    # create artists urls
-    '''
-
-'''
-for y in clean_list(songs_tag):
-    song_remove_initial = re.sub(r"\d+\. \w.* - ", "", y)
-    song_title = normalize_list(song_remove_initial)
-
-    song_url = 'https://www.tekstowo.pl/piosenka,' + artist_name + ',' + song_title + '.html'
-    
-text_tag = soup_url(song_url, "div", "class", "song-text")
-song_text = soup_song(text_tag, "p")
-translation_tag = soup_url(song_url, "div", "id", "translation")
-translation_text = soup_song(translation_tag, "p")
-'''
-# get both, translation and songtext:
-# both = soup_url(song_url, "div", "class", "tekst")
-
-# append collected data
-# records.append((song_text, translation_text, song_title, artist_name, song_url))
-# print(song_url)
-# print(artist_name)
-# print(song_title)
-# print(translation_text)
-# print(song_text)
-
-# needed for scrap pace:
-sleep(randint(2, 10))
-
-################################################################
-
-# create a data frame
-# df = pd.DataFrame(records, columns=['song_text', 'translation_text', 'song_title', 'artist_name', 'song_url'])
-# df.to_csv('tekstowo_data.csv', index=False, encoding='utf-8')
-
-# print('Data has been saved.')
-
-
-# def get_urls():
-#     for letter in alphabet:
-#         url = 'https://www.tekstowo.pl/artysci_na,' + letter
-#         for i in range(1, 201):
-#             url_page = url + ',strona,' + str(i) + '.html'
-#             # print(url_page)
-#             return url
-# get_urls()
-
-# def get_song_text():
-#
-#     songtext = soup.find("div", {"class":"song-text"})
-#     translationtext = soup.find("div", {"id":"translation"})
-#     #  get the translation and songtext:
-#     both = soup.find_all("div", {"class":"tekst"})
-#
-#     return translationtext.text
-#     # print(both.get_text())
-#
-# get_song_text()
+print('Data has been saved.')
