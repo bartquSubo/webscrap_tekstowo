5dfa0695f064266e9e9149cf7bc2cad2cb62c6cc
bartquSubo <barsub@wp.pl>
2020-06-17 07:55:32 +0100

    created a new py file for further processing

---
+++ merge_DF.py
@@ -0,0 +1,7 @@
+# print the number of missing values:
+# print(concatenated_df.isnull().sum())
+# print(concatenated_df.info())
+
+# language_detected = pd.read_csv(path + "detected_lang.csv")
+# concat_lang_songtext = pd.concat([concatenated_df, language_detected], axis=1)
+#
