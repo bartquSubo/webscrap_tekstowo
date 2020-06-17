e57a4fc3625d5e17808b040de25f9d26529f8d84
bartquSubo <barsub@wp.pl>
2020-06-17 12:30:10 +0100

    merge song_data with detected_lang ready

--- merge_DF.py
+++ merge_DF.py
@@ -1,7 +1,12 @@
-# print the number of missing values:
+import pandas as pd
+
+path = "/home/subo/PycharmProjects/webscrap_tekstowo/"
+
+language_detected = pd.read_csv(path + "detected_lang.csv")
+songs_data = pd.read_csv(path + "songs_data.csv")
+all_data = pd.concat([songs_data, language_detected], axis=1)
+all_data.to_csv("all_data.csv")
+print(all_data[["song_text", "language"]])
+
 # print(concatenated_df.isnull().sum())
 # print(concatenated_df.info())
-
-# language_detected = pd.read_csv(path + "detected_lang.csv")
-# concat_lang_songtext = pd.concat([concatenated_df, language_detected], axis=1)
-#
