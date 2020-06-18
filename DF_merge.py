786164809e32ea508c73168b296213f6f764fc52
bartquSubo <barsub@wp.pl>
2020-06-18 09:17:21 +0100

    "renaming"

---
+++ DF_merge.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+path = "/home/subo/PycharmProjects/webscrap_tekstowo/"
+
+language_detected = pd.read_csv(path + "detected_lang.csv")
+songs_data = pd.read_csv(path + "songs_data.csv")
+all_data = pd.concat([songs_data, language_detected], axis=1)
+all_data.to_csv("all_data.csv")
+print(all_data[["song_text", "language"]])
+
+# print(concatenated_df.isnull().sum())
+# print(concatenated_df.info())
